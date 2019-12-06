ca7046ba8d7fcff32f1823f8eedf1cea00d7fd94
BaeKH <bgh0611@naver.com>
2019-12-06 15:37:44 +0900

    add_20191206

---
+++ type/type_ex1.ts
@@ -0,0 +1,20 @@
+let isFruit: boolean =true;
+
+let firstNumber: number = 10;
+
+let fruit: string = "apple";
+
+let coffee: object = {
+    name  : "Americano",
+    price : 3000
+};
+
+
+function display(): void {
+    console.log("display");
+}
+
+console.log(isFruit);
+console.log(firstNumber);
+console.log(fruit);
+console.log(coffee);
---
+++ type/type_ex2.ts
@@ -0,0 +1,13 @@
+//const LINE_DRAW_TYPE = Symbol("line");
+
+let shape:any = "rectangle";
+
+console.log(shape);
+
+let colors: string[] = ["red","green","bule"];
+
+console.log(colors);
+
+function display() {
+    console.log("display");
+}
